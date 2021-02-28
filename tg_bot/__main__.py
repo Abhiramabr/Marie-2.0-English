71ef6f4e82294dcae3f11238e19e7ca87a7bb273
Abhiramabr <64951627+Abhiramabr@users.noreply.github.com>
2021-02-28 21:54:07 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,33 +18,20 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
-
-im a group manager bot maintained by  [this person](tg://user?id={}).
-
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
-
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
-
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+Hello {}, I'm a bot created to maintain [this group](T.me/cinemagalaxychat).\nFor more commands click /help
 
 """
 
 HELP_STRINGS = """
 
-Hello! my name *{}*.
-
-*Main* available commands:
- - /start: Start the bot...
- - /help: help....
- - /donate: To find out more about donating!
+Hello!
+Have a look at the following for an idea of some of the things I can help you with.
+Main commands available:
+ - /start: Start the bot
+ - /help: help
  - /settings:
-   - in PM: To find out what SETTINGS you have set....
-   - in a group:
-
+   - in PM: Will send you your settings for all supported modules.
+   - in a group: Will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
